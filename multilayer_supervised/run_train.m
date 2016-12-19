92409355517362f62f8616cf961e67b78769514a
Piotr Bandurski <piotr.bandurski@intel.com>
2016-12-19 14:30:51 +0100

    WIP mlp

--- multilayer_supervised/run_train.m
+++ multilayer_supervised/run_train.m
@@ -25,7 +25,7 @@ ei.input_dim = 784;
 % number of output classes
 ei.output_dim = 10;
 % sizes of all hidden layers and the output layer
-ei.layer_sizes = [256, ei.output_dim];
+ei.layer_sizes = [50, ei.output_dim]; % 256
 % scaling parameter for l2 weight regularization penalty
 ei.lambda = 0;
 % which type of activation function to use in hidden layers
--- multilayer_supervised/supervised_dnn_cost.m
+++ multilayer_supervised/supervised_dnn_cost.m
@@ -13,13 +13,32 @@ end;
 %% reshape into network
 stack = params2stack(theta, ei);
 numHidden = numel(ei.layer_sizes) - 1;
-hAct = cell(numHidden+1, 1);
+netSize = numel(ei.layer_sizes) + 1;
+hAct = cell(netSize, 1);
 gradStack = cell(numHidden+1, 1);
 %% forward prop
 %%% YOUR CODE HERE %%%
+hAct{1}.a = data;
+hAct{1}.z = data;
+
+for i = 1:netSize-1
+  hAct{i+1}.z = stack{i}.W * hAct{i}.a + stack{i}.b;
+  hAct{i+1}.a = sigmoid(hAct{i+1}.z);
+end
+
+% softmax
+%[f,g] = softmax_regression(stack{end}.W, hAct{end}.a,labels)
+y = labels;
+c = 1:ei.output_dim;
+I = bsxfun(@eq, y, c)';
+h = exp(hAct{end}.z);
+h_sum = sum(h, 1);
+h_sum = repmat(h_sum, ei.output_dim, 1);
+h = h ./ h_sum;
 
 %% return here if only predictions desired.
 if po
+  pred_prob = h
   cost = -1; ceCost = -1; wCost = -1; numCorrect = -1;
   grad = [];  
   return;
@@ -27,9 +46,11 @@ end;
 
 %% compute cost
 %%% YOUR CODE HERE %%%
-cost = 1000000;
+cost = -sum(sum(I.*log(h)));
+
 %% compute gradients using backpropagation
 %%% YOUR CODE HERE %%%
+gradStack{end} = -(I - h);
 
 %% compute weight penalty cost and gradient for non-bias terms
 %%% YOUR CODE HERE %%%
